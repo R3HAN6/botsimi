734c282d094ad05959cfc61a19ccdde6fd75139e
Rehan Dev <121321439+R3HAN6@users.noreply.github.com>
2023-10-13 14:50:35 +0800

    script.js

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function cari() {
+  const inputText = document.querySelector("input").value;
+  const ApiUrl = `https://api.akuari.my.id/ai/gpt?chat=${inputText}`;
+  const resultP = document.querySelector("p");
+  fetch(ApiUrl)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`Gagal Mengambil Respon Dari Api ${res.status}`);
+    }
+    return res.json();
+  })
+  .then(data => {
+    resultP.textContent = `${data["respon"]} 
+    
+Terimakasih Telah Menggunakan BOT RehanDev!!!`;
+  })
+  .catch(error => {
+    console.log(`Maaf Lagi Ada Kesalahan Sistem ${error}`);
+  })
+}
+
+const url = "https://api.github.com/users/R3HAN6";
+
+$.get(url, function (data, status) {
+  $("#profile").attr("src", data.avatar_url);
+});
+
+$(document).ready(function(){
+  $(".preloader").fadeOut();
+})
